2197e5642e1270507245ce3dad513935e7009ef2
Veeni3-Pappi <Vincenthiuhu@proton.me>
2026-06-18 17:41:23 +0300

    adding data to tables

--- bootdevl1.session.sql
+++ bootdevl1.session.sql
@@ -1,7 +1,7 @@
+INSERT INTO transactions(
+    id, recipient_id, sender_id, note, amount) VALUES
+    (1, 001, 01, 'lunch', 110.5),
+    (2, 002, 02, 'cosmetics', 2000),
+    (3, 003, 03, 'tech', 40000);
 
-
-INSERT INTO people( 
-    id, tag, name, balance, is_admin) VALUES
-    ( 1, a, Junes, 100, 1 );
-    ( 2, b, Annie, 2000.10, 0);
-;
+SELECT* FROM transactions
