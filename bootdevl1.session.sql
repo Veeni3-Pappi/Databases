377a3439442155a55044dfc04da7e797a1b32f09
Veeni3-Pappi <Vincenthiuhu@proton.me>
2026-06-18 18:05:28 +0300

    dropping tables

--- bootdevl1.session.sql
+++ bootdevl1.session.sql
@@ -1,7 +1,1 @@
-INSERT INTO transactions(
-    id, recipient_id, sender_id, note, amount) VALUES
-    (1, 001, 01, 'lunch', 110.5),
-    (2, 002, 02, 'cosmetics', 2000),
-    (3, 003, 03, 'tech', 40000);
 
-SELECT* FROM transactions
