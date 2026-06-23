d65f5529de3ee9f1b6f5ef38e15b8ff79878094b
Veeni3-Pappi <Vincenthiuhu@proton.me>
2026-06-23 16:34:03 +0300

    adding constraints to datatypes

--- bootdevl1.session.sql
+++ bootdevl1.session.sql
@@ -1,5 +1,9 @@
-ALTER TABLE transactions
-DROP COLUMN was_succesful;
-  
-ALTER TABLE transactions
-DROP COLUMN transcation_type;
+CREATE TABLE users2(
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    age INTEGER NOT NULL,
+    country_code TEXT NOT NULL, 
+    username TEXT  NOT NULL UNIQUE ,
+    password TEXT NOT NULL,
+    is_admin BOOLEAN
+);
